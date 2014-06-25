e85f865ac4bd21450d7099cf323820391fd010fc
BakaBBQ <laoboqi@126.com>
2014-06-25 22:05:13 +0800

    now fixed something..
    meow

--- android/src/com/github/bakabbq/android/AndroidLauncher.java
+++ android/src/com/github/bakabbq/android/AndroidLauncher.java
@@ -1,16 +1,6 @@
 package com.github.bakabbq.android;
 
-import android.os.Bundle;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.github.bakabbq.DanmakuStorm;
 
-public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new DanmakuStorm(), config);
-	}
+public class AndroidLauncher{
 }
